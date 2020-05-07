91770d1970a4880f0d53e7c0d2c0774f546d9069
Selim Nemsi <selim.namsi@gmail.com>
2020-05-07 12:50:22 -0400

    Fix AtScale role

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export CLOUD_TO_USE=aws
-if [ -z $AWS_ACCESS_KEY_ID ] || [-z $AWS_SECRET_ACCESS_KEY ]; then
+if [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ]; then
   echo "MUST DEFINE AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY variables"
   exit 1
 fi
 
-cluster_name="magic_cluster_$1"
+cluster_name="$1"
 
 echo "starting setting up the cloud env for cluster: $cluster_name"
 ./build_cloud.sh --extra-vars="cluster_name=$cluster_name"
--- install_atscale.sh
+++ install_atscale.sh
@@ -3,13 +3,15 @@
 source $(dirname "${BASH_SOURCE[0]}")/set_cloud.sh
 source $(dirname "${BASH_SOURCE[0]}")/set_inventory.sh
 
-if [ -z $AWS_ACCESS_KEY_ID ] || [-z $AWS_SECRET_ACCESS_KEY ]; then
+if [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ]; then
   echo "MUST DEFINE AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY variables"
   exit 1
 fi
 
+cluster_name="$1"
+
 ansible-playbook "playbooks/install_atscale.yml" \
                  --inventory="inventory/${inventory_to_use}" \
                  --extra-vars="cloud_name=${cloud_to_use}" \
-                 "$@"
+                 --extra-vars="cluster_name=${cluster_name}"
 
